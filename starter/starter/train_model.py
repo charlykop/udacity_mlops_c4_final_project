7d9d378f7c13876dd8ca2f9c7c0f603bd0d15ab0
charlykop <charly-ko@hotmail.de>
2024-01-29 08:25:40 +0100

    filled out train_model.py

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -3,8 +3,13 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import pandas as pd
+import pickle
+from ml.data import process_data
+from ml.model import train_model, compute_model_metrics, inference
 
 # Add code to load in the data.
+data = pd.read_csv('../data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -23,6 +28,16 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
+# # Proces the test data with the process_data function.
+X_test, y_test, encoder_test, lb_test = process_data(
+    test, categorical_features=cat_features, label="salary", training=False, 
+    encoder=encoder, lb=lb
+)
 
-# Train and save a model.
+# # Train and save a model.
+model = train_model(X_train, y_train)
+pickle.dump(model, open('../model/trained_model.pkl', 'wb'))
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
+print(f'precision: {precision}, recall: {recall}, F-beta: {fbeta}')
+
