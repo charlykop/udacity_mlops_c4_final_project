b20268e825d67e8abbd00aef11a24347f2753886
charlykop <charly-ko@hotmail.de>
2024-01-29 15:47:10 +0100

    Analysis of slices

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,5 +1,11 @@
+import pandas as pd
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.linear_model import LogisticRegression
+from aequitas.group import Group
+from aequitas.bias import Bias
+from aequitas.fairness import Fairness
+from aequitas.plotting import Plot
+from .data import process_data
 
 
 # Optional: implement hyperparameter tuning.
@@ -57,7 +63,7 @@ def inference(model, X):
 
     Inputs
     ------
-    model : ???
+    model : sklearn.linear_model._logistic.LogisticRegression
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -69,3 +75,64 @@ def inference(model, X):
     preds = model.predict(X)
 
     return preds
+
+def compute_sliced_performance(df, label, feature, categorical_features, model, encoder, lb):
+
+    sliced_predictions = []
+    categories = df[feature].unique()
+    for category in categories:
+        df_category = df.loc[df[feature] == category]
+        X_test, y_test, encoder_test, lb_test = process_data(
+            df_category, categorical_features=categorical_features, label=label, training=False, 
+            encoder=encoder, lb=lb
+        )
+        preds = inference(model, X_test)
+        precision, recall, fbeta = compute_model_metrics(y_test, preds)
+        sliced_predictions.append(f"Feature '{feature}' - Category: '{category}': count {len(df_category)}, precision {precision}, recall {recall}, fbeta: {fbeta}")
+        
+    return sliced_predictions
+        
+
+
+
+
+        
+
+# def compute_sliced_performance(df, label, categorical_features, preds, lb, evaluated_slice = 'education', alpha = 0.05):
+    # y = df[label]
+    # X = df.drop([label], axis=1)
+    # X = X[categorical_features]
+    # print(X.columns)
+    # try:
+    #     y = lb.transform(y.values).ravel()
+    #     # Catch the case where y is None because we're doing inference.
+    # except AttributeError:
+    #     pass
+    # df_aq = X.reset_index(drop=True).copy()
+    # df_aq['label_value'] = y
+    # df_aq['score'] = preds
+
+    # # print(pd.crosstab(df_aq.score, df_aq.sex))
+    # # print(pd.crosstab(df_aq.label_value, df_aq.sex))
+    # group = Group()
+    # xtab, idxs = group.get_crosstabs(df_aq)
+    # print(xtab.head())
+    # bias = Bias()
+    # bias_df = bias.get_disparity_major_group(xtab, original_df=df_aq, alpha=alpha, mask_significance=True)
+    # print(bias_df.head())
+    # print(bias_df.columns)
+    # fairness = Fairness()
+    # fairness_df = fairness.get_group_value_fairness(bias_df)
+    # print(fairness_df.head())
+    # overall_fairness = fairness.get_overall_fairness(fairness_df)
+    # print(overall_fairness)
+    # ap = Plot()
+    # metrics = ['fpr', 'for', 'fnr']
+    # disparity_metrics = [f"{metric}_disparity" for metric in metrics]
+    # bias_df = bias_df[["attribute_name", "attribute_value"] + metrics + disparity_metrics]
+    # eps = 0.000001
+    # bias_df[disparity_metrics] = bias_df[disparity_metrics].fillna(0.0) + eps
+    # bias_df = bias_df[["attribute_name", "attribute_value"] + metrics + disparity_metrics]
+    # print(bias_df)
+    # p = ap.plot_disparity_all(bias_df,  attributes = evaluated_slice, min_group_size= 0.01, significance_alpha=alpha)
+
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -6,9 +6,9 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 import pickle
 from ml.data import process_data
-from ml.model import train_model, compute_model_metrics, inference
+from ml.model import train_model, compute_model_metrics, inference, compute_sliced_performance
 
-# Add code to load in the data.
+# Add code to load in the data. 
 data = pd.read_csv('../data/census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
@@ -29,6 +29,7 @@ X_train, y_train, encoder, lb = process_data(
 )
 
 # # Proces the test data with the process_data function.
+
 X_test, y_test, encoder_test, lb_test = process_data(
     test, categorical_features=cat_features, label="salary", training=False, 
     encoder=encoder, lb=lb
@@ -36,8 +37,18 @@ X_test, y_test, encoder_test, lb_test = process_data(
 
 # # Train and save a model.
 model = train_model(X_train, y_train)
+print(type(model))
 pickle.dump(model, open('../model/trained_model.pkl', 'wb'))
+pickle.dump(encoder, open('../model/encoder.pkl', 'wb'))
+pickle.dump(encoder, open('../model/label_binarizer.pkl', 'wb'))
 preds = inference(model, X_test)
 precision, recall, fbeta = compute_model_metrics(y_test, preds)
 print(f'precision: {precision}, recall: {recall}, F-beta: {fbeta}')
+
+# # Performance of model based on slices
+for feature in cat_features:    
+    performance_sliced = compute_sliced_performance(test, feature=feature,label="salary", model=model, categorical_features=cat_features, encoder=encoder, lb=lb)
+    with open("slice_output.txt", 'a') as file:
+        for item in performance_sliced: 
+            file.write(item + "\n")
 
