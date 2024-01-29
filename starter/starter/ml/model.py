da3f22ac7a67aa21e345ba9b11c95dfbfc4976c0
charlykop <charly-ko@hotmail.de>
2024-01-29 16:11:49 +0100

    cleaned code and added function describtion of compute sliced performance

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -77,8 +77,33 @@ def inference(model, X):
     return preds
 
 def compute_sliced_performance(df, label, feature, categorical_features, model, encoder, lb):
+    """ Evaluates data slices using precision, recall, and F1.
 
-    sliced_predictions = []
+    Inputs
+    ------
+    df : pd.DataFrame
+        Dataframe containing the features and label
+    label : str
+        Name of the label column in `X`
+    feature: str
+        Feature to be sliced
+    categorical_features: list[str]
+        List containing the names of the categorical features
+    model : sklearn.linear_model._logistic.LogisticRegression
+        Trained machine learning model.
+    X : np.array
+        Data used for prediction.
+    encoder : sklearn.preprocessing._encoders.OneHotEncoder
+        Trained sklearn OneHotEncoder.
+    lb : sklearn.preprocessing._label.LabelBinarizer
+        Trained sklearn LabelBinarizer.
+    Returns
+    -------
+    sliced_performance: list
+        Performance of feature in slices. 
+    """
+
+    sliced_performance = []
     categories = df[feature].unique()
     for category in categories:
         df_category = df.loc[df[feature] == category]
@@ -88,51 +113,6 @@ def compute_sliced_performance(df, label, feature, categorical_features, model, 
         )
         preds = inference(model, X_test)
         precision, recall, fbeta = compute_model_metrics(y_test, preds)
-        sliced_predictions.append(f"Feature '{feature}' - Category: '{category}': count {len(df_category)}, precision {precision}, recall {recall}, fbeta: {fbeta}")
+        sliced_performance.append(f"Feature '{feature}' - Category: '{category}': count {len(df_category)}, precision {precision}, recall {recall}, fbeta: {fbeta}")
         
-    return sliced_predictions
-        
-
-
-
-
-        
-
-# def compute_sliced_performance(df, label, categorical_features, preds, lb, evaluated_slice = 'education', alpha = 0.05):
-    # y = df[label]
-    # X = df.drop([label], axis=1)
-    # X = X[categorical_features]
-    # print(X.columns)
-    # try:
-    #     y = lb.transform(y.values).ravel()
-    #     # Catch the case where y is None because we're doing inference.
-    # except AttributeError:
-    #     pass
-    # df_aq = X.reset_index(drop=True).copy()
-    # df_aq['label_value'] = y
-    # df_aq['score'] = preds
-
-    # # print(pd.crosstab(df_aq.score, df_aq.sex))
-    # # print(pd.crosstab(df_aq.label_value, df_aq.sex))
-    # group = Group()
-    # xtab, idxs = group.get_crosstabs(df_aq)
-    # print(xtab.head())
-    # bias = Bias()
-    # bias_df = bias.get_disparity_major_group(xtab, original_df=df_aq, alpha=alpha, mask_significance=True)
-    # print(bias_df.head())
-    # print(bias_df.columns)
-    # fairness = Fairness()
-    # fairness_df = fairness.get_group_value_fairness(bias_df)
-    # print(fairness_df.head())
-    # overall_fairness = fairness.get_overall_fairness(fairness_df)
-    # print(overall_fairness)
-    # ap = Plot()
-    # metrics = ['fpr', 'for', 'fnr']
-    # disparity_metrics = [f"{metric}_disparity" for metric in metrics]
-    # bias_df = bias_df[["attribute_name", "attribute_value"] + metrics + disparity_metrics]
-    # eps = 0.000001
-    # bias_df[disparity_metrics] = bias_df[disparity_metrics].fillna(0.0) + eps
-    # bias_df = bias_df[["attribute_name", "attribute_value"] + metrics + disparity_metrics]
-    # print(bias_df)
-    # p = ap.plot_disparity_all(bias_df,  attributes = evaluated_slice, min_group_size= 0.01, significance_alpha=alpha)
-
+    return sliced_performance
