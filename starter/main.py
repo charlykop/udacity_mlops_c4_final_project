7a34dc87d1b782ab4716b2dbbb824a9ba59d5af3
charlykop <charly-ko@hotmail.de>
2024-01-30 13:37:27 +0100

    Einheitliche import Methode

--- starter/main.py
+++ starter/main.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import pandas as pd
-from ml.data import process_data
+from .ml.data import process_data
 from .ml.model import inference
 
 class Data(BaseModel):
--- starter/train_model.py
+++ starter/train_model.py
@@ -5,8 +5,8 @@ from sklearn.model_selection import train_test_split
 # Add the necessary imports for the starter code.
 import pandas as pd
 import pickle
-from data import process_data
-from model import train_model, compute_model_metrics, inference, compute_sliced_performance
+from .data import process_data
+from .model import train_model, compute_model_metrics, inference, compute_sliced_performance
 
 # Add code to load in the data. 
 data = pd.read_csv('data/census.csv')
