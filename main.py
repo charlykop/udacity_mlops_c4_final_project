af293fe193b81ce29f0ca8b4d910ee84502a5ddf
charlykop <charly-ko@hotmail.de>
2024-01-30 22:07:05 +0100

    Beispiel wieder einkommentiert

--- main.py
+++ main.py
@@ -22,29 +22,29 @@ class Data(BaseModel):
     native_country: str 
 
 
-    # model_config = {
-    #     "json_schema_extra" : {
-    #         "examples": [
-    #             {
-    #                 "age": 29,
-    #                 "workclass": "State-gov",
-    #                 "fnlgt": 77516,
-    #                 "education": "Bachelors",
-    #                 "education_num": 13,
-    #                 "marital_status": "Never-married",
-    #                 "occupation": "Adm-clerical",
-    #                 "relationship": "Not-in-family",
-    #                 "race": "White",
-    #                 "sex": "Male",
-    #                 "capital_gain": 2174,
-    #                 "capital_loss": 0,
-    #                 "hours_per_week": 40,
-    #                 "native_country": "United-States"
-    #             }
-    #         ]
+    model_config = {
+        "json_schema_extra" : {
+            "examples": [
+                {
+                    "age": 29,
+                    "workclass": "State-gov",
+                    "fnlgt": 77516,
+                    "education": "Bachelors",
+                    "education_num": 13,
+                    "marital_status": "Never-married",
+                    "occupation": "Adm-clerical",
+                    "relationship": "Not-in-family",
+                    "race": "White",
+                    "sex": "Male",
+                    "capital_gain": 2174,
+                    "capital_loss": 0,
+                    "hours_per_week": 40,
+                    "native_country": "United-States"
+                }
+            ]
             
-    #     }
-    # }
+        }
+    }
     
 
 # load model, encoder and LabelBinarizer
